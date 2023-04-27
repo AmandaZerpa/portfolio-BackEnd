091dd1f1055a17a75fc2edd9cb651c870c7e8930
AmandaZerpa <amanditazerpa@gmail.com>
2023-04-27 19:14:04 -0300

    tests

--- src/test/java/com/PortfolioAmandaZerpa/SpringBoot/ApplicationTests.java
+++ src/test/java/com/PortfolioAmandaZerpa/SpringBoot/ApplicationTests.java
@@ -3,11 +3,12 @@ package com.PortfolioAmandaZerpa.SpringBoot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ApplicationTests.class)
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+            
 	}
 
 }
