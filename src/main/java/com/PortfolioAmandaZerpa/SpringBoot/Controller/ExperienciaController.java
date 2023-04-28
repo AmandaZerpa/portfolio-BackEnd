6d559017086217d6871d2021dfc99c86b80a869e
AmandaZerpa <amanditazerpa@gmail.com>
2023-04-27 22:05:37 -0300

    cors

--- src/main/java/com/PortfolioAmandaZerpa/SpringBoot/Controller/ExperienciaController.java
+++ src/main/java/com/PortfolioAmandaZerpa/SpringBoot/Controller/ExperienciaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"https://mgbfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://azportafolio-19a6e.web.app/","http://localhost:4200"})
 public class ExperienciaController {
     @Autowired
     ImpExperienciaService impExperienciaService;
--- src/main/java/com/PortfolioAmandaZerpa/SpringBoot/Controller/PersonaController.java
+++ src/main/java/com/PortfolioAmandaZerpa/SpringBoot/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://azportafolio-19a6e.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://azportafolio-19a6e.web.app/","http://localhost:4200"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
