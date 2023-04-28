c0c2ffa3b79c0ea3c92c97b63259095e26e14c60
AmandaZerpa <amanditazerpa@gmail.com>
2023-04-27 21:48:30 -0300

    application properties

--- src/test/java/com/PortfolioAmandaZerpa/SpringBoot/ApplicationTests.java
+++ src/test/java/com/PortfolioAmandaZerpa/SpringBoot/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.PortfolioAmandaZerpa.SpringBoot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ApplicationTests.class)
+@SpringBootTest
 class ApplicationTests {
 
 	@Test
