41764d527152e7c63821ff5198b88cd031590d8c
AmandaZerpa <amanditazerpa@gmail.com>
2023-05-10 23:16:02 -0300

    cors security

--- src/main/java/com/PortfolioAmandaZerpa/SpringBoot/Security/MainSecurity.java
+++ src/main/java/com/PortfolioAmandaZerpa/SpringBoot/Security/MainSecurity.java
@@ -3,12 +3,15 @@ package com.PortfolioAmandaZerpa.SpringBoot.Security;
 import com.PortfolioAmandaZerpa.SpringBoot.Security.Service.UserDetailsImp;
 import com.PortfolioAmandaZerpa.SpringBoot.Security.jwt.JwtEntryPoint;
 import com.PortfolioAmandaZerpa.SpringBoot.Security.jwt.JwtTokenFilter;
+import java.time.Duration;
+import java.util.Arrays;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +21,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -39,10 +45,26 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
         return new BCryptPasswordEncoder();
     }
     
+    @Bean
+ CorsConfigurationSource corsConfigurationSource() {
+  CorsConfiguration cc = new CorsConfiguration();
+                cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers","Authorization"));
+                cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));                
+                cc.setAllowedOrigins(Arrays.asList("/*"));
+                cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH"));
+                cc.addAllowedOrigin("*");
+                cc.setMaxAge(Duration.ZERO);
+                cc.setAllowCredentials(Boolean.TRUE);
+  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  source.registerCorsConfiguration("/**", cc);
+  return source;
+ }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeHttpRequests()
+        http
+            .cors(withDefaults())
+            .csrf().disable().authorizeHttpRequests()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
